34d323ee06bc7b6bc7e7857792df24b48afa8903
Nguyễn Thị Bích Ngọc <35924978+Aiko419@users.noreply.github.com>
2021-06-13 23:45:44 +0700

    login form first

---
+++ src/components/Login.tsx
@@ -0,0 +1,127 @@
+import React, { useReducer, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+type State = {
+  username: string
+  password:  string
+  isButtonDisabled: boolean
+  helperText: string
+  isError: boolean
+};
+
+const initialState:State = {
+  username: '',
+  password: '',
+  isButtonDisabled: true,
+  helperText: '',
+  isError: false
+};
+
+type Action = { type: 'setUsername', payload: string }
+  | { type: 'setPassword', payload: string }
+  | { type: 'setIsButtonDisabled', payload: boolean }
+  | { type: 'loginSuccess', payload: string }
+  | { type: 'loginFailed', payload: string }
+  | { type: 'setIsError', payload: boolean };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'setUsername': 
+      return {
+        ...state,
+        username: action.payload
+      };
+    case 'setPassword': 
+      return {
+        ...state,
+        password: action.payload
+      };
+    case 'setIsButtonDisabled': 
+      return {
+        ...state,
+        isButtonDisabled: action.payload
+      };
+    case 'loginSuccess': 
+      return {
+        ...state,
+        helperText: action.payload,
+        isError: false
+      };
+    case 'loginFailed': 
+      return {
+        ...state,
+        helperText: action.payload,
+        isError: true
+      };
+    case 'setIsError': 
+      return {
+        ...state,
+        isError: action.payload
+      };
+  }
+}
+
+const Login = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    if (state.username.trim() && state.password.trim()) {
+     dispatch({
+       type: 'setIsButtonDisabled',
+       payload: false
+     });
+    } else {
+      dispatch({
+        type: 'setIsButtonDisabled',
+        payload: true
+      });
+    }
+  }, [state.username, state.password]);
+
+  const handleLogin = () => {
+    if (state.username === 'admin' && state.password === 'admin') {
+      dispatch({
+        type: 'loginSuccess',
+        payload: 'Login Successfully'
+      });
+    } else {
+      dispatch({
+        type: 'loginFailed',
+        payload: 'Incorrect username or password'
+      });
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.keyCode === 13 || event.which === 13) {
+      state.isButtonDisabled || handleLogin();
+    }
+  };
+
+  const handleUsernameChange: React.ChangeEventHandler<HTMLInputElement> =
+    (event) => {
+      dispatch({
+        type: 'setUsername',
+        payload: event.target.value
+      });
+    };
+
+  const handlePasswordChange: React.ChangeEventHandler<HTMLInputElement> =
+    (event) => {
+      dispatch({
+        type: 'setPassword',
+        payload: event.target.value
+      });
+    }
+  return (
+    <>
+         <TextField onChange={handleUsernameChange} onKeyPress={handleKeyPress} error={state.isError} className="full-width"  label="User Name" id="username" /><br/><br/>
+         <TextField onChange={handlePasswordChange} onKeyPress={handleKeyPress} error={state.isError} helperText={state.helperText} className="full-width"  label="Password" id="password" type="password" /> <br/><br/>
+         <Button onClick={handleLogin} disabled={state.isButtonDisabled}  className="full-width"  variant="contained" color="secondary">Login</Button>
+    </>
+  );
+}
+
+export default Login;
